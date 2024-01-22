86e39abdbecba471055bace7a87337a3b1529e30
ibanespm <ibanesp59@gmail.com>
2024-01-21 22:04:33 -0500

    portfolio

---
+++ docs/index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Developer Fullstack", "Etical Hacking", "Machine Learning"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    startDelay: 1000,
+    loop: true
+});
